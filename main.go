4acea9088e0a0c6bc4db8a17a87fbcc115efbbce
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-01-21 15:27:19 -0500

    totp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
--- totp.go
+++ totp.go
@@ -1,4 +1,26 @@
 package main
 
 
+import (
+	"fmt"
+	"math"
+	"time"
+)
 
+
+/* TOTP = HOTP(K, T)
+ * where T = is an integer and represents the
+ * number of time steps between the initial counter
+ * time T0 and the current Unix time
+ */
+type TOTP struct {
+	Timestep uint8
+	hotp HOTP
+}
+
+
+func  (totp TOTP) Generate() uint {
+	totp.hotp.Counter = (uint64(time.Now().Unix()) / uint64(totp.Timestep))
+
+	return totp.hotp.Generate()
+}
